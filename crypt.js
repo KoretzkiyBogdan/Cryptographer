d12516c238e1a0d17b2d0655c741c833cb0cdc9c
Koretzkiy Bogdan <koretzkiy.bogdan@gmail.com>
2016-02-14 20:13:16 +0200

    Add another crypt method and test

--- crypt.js
+++ crypt.js
@@ -1,9 +1,11 @@
 var crypt = (function() {
 
   // Random symbols
-  var letters = ['g','i','j','k','l','m', 'n','o', 'p', 'q', 'r', 's', 'x', 'y', 'z'];
-  // flag symbols
-  var spec = ['w', 'h', 't', 'u', 'v', 'w'];
+  var letters = ['i','k','l','m', 'n','o', 'p', 'q', 'r', 's', 'x', 'y', 'z'];
+  // flag symbols, that means as value must be reversed
+  var specReverse = ['h', 't', 'u'];
+  //flag symbols, that means as value symbor must be charCode
+  var specChar = ['v', 'w', 't', 'g', 'j'];
 
   var getTrueOrFalse = function() {
     return !!Math.round(Math.random());
@@ -23,8 +25,13 @@ var crypt = (function() {
   }
 
   var wrappedInRandomletters = function(char) {
+    specChar.forEach(function(spec) {
+      if (spec.charCodeAt(0) == parseInt(char, 16)) {
+        char = spec;
+      }
+    });
 
-    var charList = getTrueOrFalse() ? reverseValue(char) + getRandomChar(spec) : char;
+    var charList = getTrueOrFalse() ? reverseValue(char) + getRandomChar(specReverse) : char;
     var countLetters = getRandomInteger(3,1);
 
     for (var i = 0; i < countLetters; i++) {
@@ -73,16 +80,21 @@ var crypt = (function() {
       return new Error('You must put key as second parameter');
     }
 
-    var exp = new RegExp('[a-f]?\\d{1,2}([a-f' + spec.join('') + '])?', 'gi');
-    var specExp = new RegExp('[' + spec.join('') + ']', 'i');
+    var exp = new RegExp('[a-f]?(\\d{1,2}|[' + specChar.join('') + '])([a-f' + specReverse.join('') + '])?', 'gi');
+    var specReverseExp = new RegExp('[' + specReverse.join('') + ']', 'i');
+    var specCharExp = new RegExp('[' + specChar.join('') + ']', 'i');
 
     var keys = getEveryCharacterCodeArray(key);
 
+
     return text.match(exp).map(function(char) {
       //if match spec symbols, then need remove that symbols and return reversed value
-      if (char.match(specExp)) {
-        char = reverseValue(char.replace(specExp, ''));
+      if (char.match(specReverseExp)) {
+        char = reverseValue(char.replace(specReverseExp, ''));
       };
+      if (char.match(specCharExp)) {
+        char = char.charCodeAt(0).toString(16);
+      }
       char = bitwiseXOR(parseInt(char, 16), keys);
       return String.fromCharCode(char);
     }).join('');
@@ -93,22 +105,3 @@ var crypt = (function() {
     parse: parse
   };
 })();
-
-var message = 'Hello, I love you!',
-    key = 'qwvvu4556';
-
-for (var i = 0; i < 1000; i++) {
-  var test = crypt.create(message, key);
-  var test = crypt.parse(test, key);
-  console.log(test === message);
-}
-
-console.log('origin message: \n'.toUpperCase(), message);
-
-message = crypt.create(message, key);
-
-console.log('cryptographic message: \n'.toUpperCase(), message);
-
-message = crypt.parse(message, key);
-
-console.log('parsing message: \n'.toUpperCase(), message);
